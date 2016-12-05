a07e84182b51086b8b7768f9509d82053b020d86
dsummers91 <deonsummers91@gmail.com>
2016-12-05 01:39:59 -0600

    rename variables

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,9 @@
-var adults = [web3.eth.accounts[0], web3.eth.accounts[1]]
-var children = [web3.eth.accounts[2], web3.eth.accounts[3], web3.eth.accounts[4]]
+var adults = [web3.eth.accounts[0], web3.eth.accounts[1]];
+var children = [web3.eth.accounts[2], web3.eth.accounts[3], web3.eth.accounts[4]];
+var defaultAllowanceInEth = 15;
 var durationInDays = 1;
+var includeFirstAllowance = 1;
 
 module.exports = function(deployer) {
-  deployer.deploy(FamilyWallet, adults, children, durationInDays);;
+  deployer.deploy(FamilyWallet, adults, children, defaultAllowanceInEth,durationInDays, includeFirstAllowance);
 };
